41640f69e3532bd9b88875ca6e745c12f43c9118
carlos <qzq.pers@msn.com>
2016-10-17 17:06:05 +0800

    A: svn confi and svn project check

--- patch.py
+++ patch.py
@@ -6,10 +6,13 @@ import sys,time
 import subprocess
 import threading
 
+SCRIPT_VERSION = '0.1'
+
 #text color
 highline = '\033[1m'
 green = '\033[1;32m'
 red = '\033[1;31m'
+yellow = '\033[0;33m'
 default = '\033[0m'
 
 global showprogress
@@ -41,78 +44,107 @@ def progress(msg):
 		t += 1
 		time.sleep(0.5)
 	if result[0] == 0:
-		sys.stdout.write(msg + '[' + green + '\u2714'.encode("utf-8").decode('unicode_escape') + default +']\n')
+		sys.stdout.write(msg + '[' + green + '✔' + default +']\n')
 	else:
-		sys.stdout.write(msg + '[' + red + '\u2715'.encode("utf-8").decode('unicode_escape') + default +']\n')
+		sys.stdout.write(msg + '[' + red + '✕' + default +']\n')
+
 def doinbackground(cmd, msg):
 	threading.Thread(target=cmdtask, args=(cmd,)).start()
 	progress(msg)
 
 repo_url = ''
 
-svn_username = 'zhouqing.qiu'
-svn_password = 'zhouqing.qiu'
+svn_username = ''
+svn_password = ''
 
-work_path = '/home/Disk_2TB/qiuzhouqing/MTK_RELEASE'
-project_folder = 'MT6750'
-alps_dir = work_path + '/' + project_folder
+work_path = os.getcwd()
+alps_dir = ''
 log_name = 'svn_log.txt'
 
 tar_command = 'tar -xvzf '
 cp_command = 'cp -rvf '
 rm_command = 'rm -rvf '
-
-svn_update_command = 'svn update --username ' + svn_username + ' --password ' + svn_password
+svn_update_command = 'svn update'
 svn_add_command = "svn st --no-ignore | awk '{if($1 == \"?\"){print $2}}' | xargs -r svn add"
 svn_commit_command = 'svn commit --file '
-
-patches = []
 
 #SVN MESSAGE
 svn_msg = '模块：patch\n修改点：%s\n%s'
 
-#into project folder
-#print(green)
-#info('------------------------------ INTO FOLDER ----------------------------')
-#print(alps_dir)
-#result = commands.getstatusoutput('cd ' + alps_dir)
-#if result[0]:
-#	print(red + result[1] + default)
-#	exit()
-#update code
-#info('------------------------------ UPDATE CODE ----------------------------')
-os.chdir(alps_dir)
-#doinbackground(svn_update_command, '#update    ')
-info('------------------------------ PATCHES LIST----------------------------')
-#print('------------------------------ PATCHES LIST----------------------------')
-for f in os.listdir(work_path):
-	if os.path.isfile(work_path + '/' + f) and f.endswith('tar.gz'):
-		patches.append(f)
-		#print(' ' * 4 + f)
-		#info('-----------------------------------------------------------------------')
-patches.sort()
-for p in patches:
-	print(' ' * 4 + p)
-	#print('-----------------------------------------------------------------------')
-	info('-----------------------------------------------------------------------')
-in_msg = raw_input(highline + '\nAre you sure the patches\'s sequence is right, it is important (yes/no): ' + default)
+print(green + '--------------------- MTK AUTO PATCH SCRIPT -------------------------' + default)
 
-while True:
-	if 'no' == in_msg:
-		info('Script will exit, thx for using!')
+# SVN project
+alps_dir = ''
+svn_projects = []
+dirs = [dir for dir in os.listdir(work_path) if os.path.isdir(work_path + '/' + dir)]
+for dir in dirs:
+    for file in os.listdir(work_path + '/' + dir):
+        if file == '.svn':
+            svn_projects.append(dir)
+if len(svn_projects) > 0:
+    info('#found dirs')
+    number = 0
+    for project in svn_projects:
+        number += 1
+        print('    ' + str(number) + '. ' + work_path + '/' + project);
+    u_in = input(yellow + 'Please choose a dir as your svn project(choose \'0\' to input path): ' + default)
+    if u_in > 0:
+        alps_dir = work_path + '/' + svn_projects[u_in-1]
+    else:
+        alps_dir = raw_input('Please input svn project path: ')
+else:
+    alps_dir = aw_input(yellow + 'Not found svn project, Please input path: ' + default)
+
+# show svn config
+svn_config_path = os.environ['HOME'] + '/' + '.subversion/auth/svn.simple'
+files = os.listdir(svn_config_path)
+if len(files) == 1 and os.path.isfile(svn_config_path + '/' + files[0]):
+    svn_config_path += '/' + files[0]
+    svn_config = open(svn_config_path, 'r')
+    svn_line = 0
+    svn_username_line = 0
+    svn_password_line = 0
+    for line in svn_config.readlines():
+        svn_line += 1
+        if svn_line == svn_username_line:
+            svn_username = line[:len(line) -1:]
+        elif svn_line == svn_password_line:
+            svn_password = line[:len(line) -1:]
+        if line.find('username') != -1:
+            svn_username_line = 2 + svn_line
+        elif line.find('password') != -1:
+            svn_password_line = 2 + svn_line
+    svn_config.close()
+info('#svn config')
+print('    --username: ' + svn_username)
+print('    --password: ' + svn_password)
+if raw_input(yellow + 'Do you want to change the svn account?(yes/no): ' + default) == 'yes':
+    svn_username = raw_input('svn username: ')
+    svn_password = raw_input('svn_password: ')
+    svn_update_command = 'svn update --username ' + svn_username + ' --password ' + svn_password
+
+#update code
+info('#update code')
+os.chdir(alps_dir)
+doinbackground(svn_update_command, '    --update    ')
+patches = [f for f in os.listdir(work_path) if os.path.isfile(work_path + '/' + f) and f.endswith('tar.gz')]
+patches.sort()
+if len(patches) > 0:
+	info('#patch list')
+	for p in patches:
+		print('    ' + p)
+	if 'yes' != raw_input(yellow + 'Are you sure the patches\'s sequence is right, it is important (yes/no): ' + default):
 		exit()
-	elif 'yes' != in_msg:
-		error('Please input "yes" or "no"')
-		in_msg = raw_input(highline+'\nplease check the patches\'s sequence!!!!(yes/no): '+default)
-	else: 
-		break
+else:
+	error('Not found patches files, execute over!')
+	exit()
 
 for patch in patches:
-	info('\n-------- ' + patch[patch.rfind('_')+1 : patch.rfind(')') : 1] + ' --------')
+	info('-------- ' + patch[patch.rfind('_')+1 : patch.rfind(')') : 1] + ' --------')
 	m_patch = patch
 	m_patch = m_patch.replace('(', '\(')
 	m_patch = m_patch.replace(')', '\)')
-	print('#file operation')
+	info('#file operation')
 	#begin unzip patch file
 	os.chdir(work_path)
 	doinbackground(tar_command + work_path + '/' + m_patch, '    --unzip     ')
@@ -125,9 +157,9 @@ for patch in patches:
 		error(red + result[1])
 		break
 	#svn add
-	print('#svn operation')
+	info('#svn operation')
 	os.chdir(alps_dir)
-	#doinbackground(svn_add_command, '    --add       ')
+	doinbackground(svn_add_command, '    --add       ')
 	if result[0]:
 		error(result[1])
 		break
@@ -139,17 +171,17 @@ for patch in patches:
 	patch_list.close()
 	svn_log.close()
 	#commit
-	#doinbackground(svn_commit_command + work_path + '/svn_log.txt' , '    --commit    ')
+	doinbackground(svn_commit_command + work_path + '/' + log_name , '    --commit    ')
 	if result[0]:
 		error(result[1])
 		break
 	#delete unzip files
-	print('#finished')
+	info('#finished')
 	os.chdir(work_path)
 	doinbackground(rm_command + 'alps/ patch_list.txt ' + log_name, '    --rm cache  ')
 
-if 'yes' == raw_input(highline+'\nMTK Patch Finished, want to delete the patch tar? (yes/no): '+default):
+if 'yes' == raw_input(yellow+'MTK Patch Finished, want to delete the patch tar? (yes/no): '+default):
 	for patch in patches:
 		os.remove(work_path + '/' + patch)
 
-info('------------------------- THANK YOU FOR USING -----------------------')
+print(green+'------------------------- THANK YOU FOR USING -----------------------'+default)
